a5c542b7f09f2b66364eb1b38447cafb49ae541d
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-19 02:43:13 +1000

    Lab 1.2

---
+++ labs/lab1/part2/part2.vhd
@@ -0,0 +1,30 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY part2 IS
+  PORT (
+	 SW    : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+    LEDG  : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+  );
+  
+  SIGNAL s : STD_LOGIC_VECTOR(3 DOWNTO 0);
+  -- ALIAS s IS SW(9);
+  
+  ALIAS M IS LEDG(3 DOWNTO 0);
+  ALIAS X IS SW(3 DOWNTO 0);
+  ALIAS Y IS SW(7 DOWNTO 4);
+  -- ALIAS Y : STD_LOGIC_VECTOR(3 DOWNTO 0) IS SW(7 DOWNTO 4);
+    
+END part2;
+
+
+ARCHITECTURE Behaviour OF part2 IS
+BEGIN
+  s <= SW(9) & SW(9) & SW(9) & SW(9);
+  LEDG <= (NOT(s) AND X) OR (s AND Y);
+
+  -- LEDG(0) <= (NOT(s) AND X(0)) OR (s AND Y(0));
+  -- LEDG(1) <= (NOT(s) AND X(1)) OR (s AND Y(1));
+  -- LEDG(2) <= (NOT(s) AND X(2)) OR (s AND Y(2));
+  -- LEDG(3) <= (NOT(s) AND X(3)) OR (s AND Y(3));
+END Behaviour;
