e8e7e2c5a221d38e47cf8a16dedd316e11adfeb7
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-19 02:52:14 +1000

    Use M instead of LEDG

--- labs/lab1/part2/part2.vhd
+++ labs/lab1/part2/part2.vhd
@@ -21,18 +21,18 @@ END part2;
 ARCHITECTURE Behaviour OF part2 IS
 BEGIN
   s <= SW(9) & SW(9) & SW(9) & SW(9);
-  LEDG <= (NOT(s) AND X) OR (s AND Y);
+  M <= (NOT(s) AND X) OR (s AND Y);
 
   -- PROCESS (s) BEGIN
   --   IF s = '0' THEN
-  --     LEDG <= X;
+  --     M <= X;
   --   ELSE
-  --     LEDG <= Y;
+  --     M <= Y;
   --   END IF;
   -- END PROCESS;
 
-  -- LEDG(0) <= (NOT(s) AND X(0)) OR (s AND Y(0));
-  -- LEDG(1) <= (NOT(s) AND X(1)) OR (s AND Y(1));
-  -- LEDG(2) <= (NOT(s) AND X(2)) OR (s AND Y(2));
-  -- LEDG(3) <= (NOT(s) AND X(3)) OR (s AND Y(3));
+  -- M(0) <= (NOT(s) AND X(0)) OR (s AND Y(0));
+  -- M(1) <= (NOT(s) AND X(1)) OR (s AND Y(1));
+  -- M(2) <= (NOT(s) AND X(2)) OR (s AND Y(2));
+  -- M(3) <= (NOT(s) AND X(3)) OR (s AND Y(3));
 END Behaviour;
