d061b94ed0219980a630ec349fa04fb42c8d106f
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-20 02:30:42 +1000

    Use vector logic

--- labs/lab1/part3/L1P3.vhd
+++ labs/lab1/part3/L1P3.vhd
@@ -11,6 +11,7 @@ END L1P3;
 ARCHITECTURE behaviour OF L1P3 IS
 	SIGNAL U, V, W, M, N	: STD_LOGIC_VECTOR(1 DOWNTO 0);
 	SIGNAL s : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	SIGNAL t : STD_LOGIC_VECTOR(1 DOWNTO 0); -- Added an extra signal to contain a "SW(9) is HIGH vector"
 BEGIN
 	LEDG(9 DOWNTO 2) <= "00000000";
 	LEDG(1 DOWNTO 0) <= M;
@@ -18,14 +19,18 @@ BEGIN
 	U <= SW(5 DOWNTO 4);
 	V <= SW(3 DOWNTO 2);
 	W <= SW(1 DOWNTO 0);
-	s <= SW(9 DOWNTO 8);
+	
+	-- s <= SW(9 DOWNTO 8);
+	s <= SW(8) & SW(8);
+	t <= SW(9) & SW(9);
 	
 	-- expressions involving elements of s, U and V
-	N(0) <= (NOT(s(0)) AND U(0)) OR (s(0) AND V(0));
-	N(1) <= (NOT(s(0)) AND U(1)) OR (s(0) AND V(1));
-	
+	N <= (NOT(s) AND U) OR (s AND V);
+	-- N(0) <= (NOT(s(0)) AND U(0)) OR (s(0) AND V(0));
+   -- N(1) <= (NOT(s(0)) AND U(1)) OR (s(0) AND V(1));
+
 	-- expressions involving elements of s, N and W
-	M(0) <= (NOT(s(1)) AND N(0)) OR (s(1) AND W(0));
-	M(1) <= (NOT(s(1)) AND N(1)) OR (s(1) AND W(1));
+	M <= (NOT(t) AND N) OR (t AND W);
+	
 	
 END behaviour;
