ffff70bc22e215cf16b3324cac4fcc7564ebf01f
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-30 01:34:12 +1000

    Formatting

--- labs/lab2/part1/L2P1.vhd
+++ labs/lab2/part1/L2P1.vhd
@@ -2,38 +2,39 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY L2P1 IS
-	PORT(	SW	:IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-			HEX0	:OUT STD_LOGIC_VECTOR(0 TO 6);
-			HEX1	:OUT STD_LOGIC_VECTOR(0 TO 6));
+  PORT (
+    SW   : IN  STD_LOGIC_VECTOR(9 DOWNTO 0);
+	 HEX0 : OUT STD_LOGIC_VECTOR(0 TO 6);
+	 HEX1 : OUT STD_LOGIC_VECTOR(0 TO 6);
+
+	 -- For clearing the display
+	 HEX2 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	 HEX3 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+  );
 END L2P1;
 
 ARCHITECTURE structure OF L2P1 IS
-	COMPONENT bcd_to_hex IS
-		PORT (B	: IN std_logic_vector(3 DOWNTO 0);
-				H	: OUT std_logic_vector(0 TO 6));
-	END COMPONENT;
+  COMPONENT bcd_to_hex IS
+    PORT (
+	   B : IN  std_logic_vector(3 DOWNTO 0);
+	   H : OUT std_logic_vector(0 TO 6)
+	 );
+  END COMPONENT;
 BEGIN
+  -- Turn off HEX3 and HEX2
+  HEX2 <= "1111111";
+  HEX3 <= "1111111";
 
-	h0: -- your bcd_to_hex instantiations
-	h1: 
-	
-END structure;	
-
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-
-ENTITY bcd_to_hex IS
-	PORT	(B	:IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			 H	:OUT STD_LOGIC_VECTOR(0 TO 6));
-END bcd_to_hex;
-
-ARCHITECTURE logicfunc OF bcd_to_hex IS
-BEGIN
-	H(0) <= -- your Boolean logic expression
-	H(1) <= 
-	H(2) <= 
-	H(3) <= 
-	H(4) <= 
-	H(5) <= 
-	H(6) <= 
-END logicfunc;
+  -- Assign ports to HEX1
+  h1: bcd_to_hex PORT MAP (
+    B => SW(7 DOWNTO 4),
+    H => HEX1
+  );
+  
+  -- Assign ports to HEX0
+  h0: bcd_to_hex PORT MAP (
+    B => SW(3 DOWNTO 0),
+    H => HEX0
+  );
+  
+END structure;  
---
+++ labs/lab2/part1/L2P1.vhd.bak
@@ -0,0 +1,39 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY L2P1 IS
+	PORT(	SW	:IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			HEX0	:OUT STD_LOGIC_VECTOR(0 TO 6);
+			HEX1	:OUT STD_LOGIC_VECTOR(0 TO 6));
+END L2P1;
+
+ARCHITECTURE structure OF L2P1 IS
+	COMPONENT bcd_to_hex IS
+		PORT (B	: IN std_logic_vector(3 DOWNTO 0);
+				H	: OUT std_logic_vector(0 TO 6));
+	END COMPONENT;
+BEGIN
+
+	h0: -- your bcd_to_hex instantiations
+	h1: 
+	
+END structure;	
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY bcd_to_hex IS
+	PORT	(B	:IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			 H	:OUT STD_LOGIC_VECTOR(0 TO 6));
+END bcd_to_hex;
+
+ARCHITECTURE logicfunc OF bcd_to_hex IS
+BEGIN
+	H(0) <= -- your Boolean logic expression
+	H(1) <= 
+	H(2) <= 
+	H(3) <= 
+	H(4) <= 
+	H(5) <= 
+	H(6) <= 
+END logicfunc;
