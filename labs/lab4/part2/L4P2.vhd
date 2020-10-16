c09d1f7e7ac0c5faff9673e112e7eb28876b1c63
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-17 01:38:55 +1100

    Lab 4.2

--- labs/lab4/part2/L4P2.vhd
+++ labs/lab4/part2/L4P2.vhd
@@ -3,13 +3,20 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 
 ENTITY L4P2 IS
-	PORT(	E, Clk, Clearn: IN std_logic;
-			Q : BUFFER std_logic_vector(15 DOWNTO 0));
+	PORT (
+		E, Clk, Clearn : IN 		std_logic;
+		Q 					: BUFFER std_logic_vector(15 DOWNTO 0) -- or 7 DOWNTO 0 for 8-bit
+	);
 END L4P2;
 
 ARCHITECTURE behavioural OF L4P2 IS
 BEGIN
-
-	-- your VHDL code
+	PROCESS (Clk, Clearn) BEGIN
+		IF Clearn = '0' THEN
+			Q <= (OTHERS => '0');
+		ELSIF Clk'event AND Clk = '1' AND E = '1' THEN
+			Q <= Q + 1;
+		END IF;
+	END PROCESS;
 	
 END behavioural;
