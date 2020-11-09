ca02b1465564a1932639998dde2719f73cf8010a
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-09 15:30:16 +1100

    Add entities

---
+++ files/components/addSubUnit.vhd
@@ -0,0 +1,28 @@
+LIBRARY ieee; 
+USE ieee.std_logic_1164.all;
+
+ENTITY addSubUnit IS
+	GENERIC (n : INTEGER := 9);
+	PORT (
+		P1, P2		: IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+		AddOrSub		: IN STD_LOGIC; -- 0 for add, 1 for subtract
+		Clock  		: IN STD_LOGIC;
+		O 				: BUFFER STD_LOGIC_VECTOR(n-1 DOWNTO 0)
+	);
+
+END addSubUnit;
+
+ARCHITECTURE behaviour OF addSubUnit IS
+	PROCESS (Clock) BEGIN
+		IF Clock'event AND Clock = '1' THEN
+			-- O <= P1 + P2 WHEN AddOrSub = '0' ELSE P1 - P2;
+			IF AddOrSub = '0' THEN
+				O <= P1 + P2;
+			ELSE
+				O <= P1 - P2;
+			END IF;
+		END IF;
+	END PROCESS;
+
+END behaviour;
+
--- labs/lab9/L9P1/L9P1sim.vhd
+++ labs/lab9/L9P1/L9P1sim.vhd
@@ -4,65 +4,110 @@ USE ieee.std_logic_signed.all;
 
 ENTITY L9P1sim IS
 	PORT (
-		DIN 					 : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
-		Resetn, Clock, Run	 : IN STD_LOGIC;
-		Done 					 : BUFFER STD_LOGIC;
-		BusWires 			 : BUFFER STD_LOGIC_VECTOR(8 DOWNTO 0));
+		DIN 					 : IN STD_LOGIC_VECTOR(8 DOWNTO 0);			-- Data
+		Resetn, Clock, Run	 : IN STD_LOGIC;									-- Control
+		Done 					 : BUFFER STD_LOGIC;								-- DONE state
+		BusWires 			 : BUFFER STD_LOGIC_VECTOR(8 DOWNTO 0)		-- Mux output
+	);
 END L9P1sim;
 
 ARCHITECTURE Mixed OF part1 IS
-	-- declare components
-	-- declare signals
+	-- :: Signals :: --
+	--SIGNAL IR				: STD_LOGIC_VECTOR(8 DOWNTO 0);	-- Instruction register; IIIXXXYYY
+	SIGNAL instruction	: STD_LOGIC_VECTOR(2 DOWNTO 0);	-- 3-bit instruction
+	SIGNAL enableDecode	: STD_LOGIC; -- Enable 3-to-8 bit decoder
+	
+	SIGNAL enableReg		: STD_LOGIC_VECTOR(0 TO 11); -- Enable register writes
+	ALIAS enableA	IS enableReg(9);
+	ALIAS enableG	IS enableReg(10);
+	ALIAS enableIR	IS enableReg(11);
+	
+	SIGNAL BusWires: STD_LOGIC_VECTOR(8 DOWNTO 0);
+	
+	-- :: Outputs :: --
+	SIGNAL AddSubOutput IS STD_LOGIC_VECTOR(8 DOWNTO 0);
+	
+	SIGNAL R: ARRAY (0 TO 11) OF STD_LOGIC_VECTOR(8 DOWNTO 0); -- https://stackoverflow.com/a/9702456
+	ALIAS A IS R(9);
+	ALIAS G IS R(10);
+	ALIAS IR IS R(11);
+	
+	-- :: Selects :: --
+	SIGNAL addSubMode 	: STD_LOGIC;
+	SIGNAL useG, useDIN	: STD_LOGIC;
+	SIGNAL regX, regY		: STD_LOGIC_VECTOR(7 DOWNTO 0); -- Register select for multiplexer
+	
+	-- :: States :: --
 	TYPE State_type IS (T0, T1, T2, T3);
 	SIGNAL Tstep_Q, Tstep_D: State_type;
-	--
 BEGIN
-	Hi <= '1';
-	I <= IR(8 DOWNTO 6);
-	decX: work.dec3to8 PORT MAP (IR(5 DOWNTO 3), Hi, Xreg);
-	decY: work.dec3to8 PORT MAP (IR(2 DOWNTO 0), Hi, Yreg);
+	-- :: Components :: --
+	decX: work.dec3to8 PORT MAP (IR(5 DOWNTO 3), Hi, regX);
+	decY: work.dec3to8 PORT MAP (IR(2 DOWNTO 0), Hi, regY);
+	
+	REGISTERS: 	FOR i in 0 to 9 GENERATE
+						-- Include Register A
+						BUS_IN_REGS: ENTITY work.regn PORT MAP (
+							BusWires, enableReg(i), Clock, R(i)
+						);
+					END GENERATE;
+	REGISTER_G: ENTITY work.regn PORT MAP (
+						AddSubOutput, enableG, Clock, R(10)
+					);
+	REGISTER_IR: ENTITY work.regn PORT MAP (
+					   DIN, enableIR, Clock, R(11)
+					 );
+												
+	ADD_SUB: ENTITY work.addSubUnit PORT MAP (
+				  A, BusWires, addSubMode, Clock, AddSubOutput
+				);
 
+	enableDecode <= '1';
+	instruction <= IR(8 DOWNTO 6);
+	
 	statetable: PROCESS (Tstep_Q, Run, Done)
-	BEGIN
-		CASE Tstep_Q IS
-			WHEN T0 => -- data is loaded into IR in this time step
-				IF (Run = '0') THEN
-					Tstep_D <= T0;
-				ELSE 
-					Tstep_D <= T1;
-				END IF; 
-			-- other states
-		END CASE;
-	END PROCESS;
+		BEGIN
+			CASE Tstep_Q IS
+				WHEN T0 => -- data is loaded into IR in this time step
+					IF (Run = '1') THEN
+						Tstep_D <= T1;
+					ELSE 
+						Tstep_D <= T0;
+					END IF; 
+				WHEN T1 =>
+				WHEN T2 =>
+				WHEN T3 =>
+			END CASE;
+		END PROCESS;
 
-	controlsignals: PROCESS (Tstep_Q, I, Xreg, Yreg)
-	BEGIN
-		-- specify initial values
-		CASE Tstep_Q IS
-			WHEN T0 => -- store DIN in IR as long as Tstep_Q = 0
-				IRin <= '1';
-			WHEN T1 => -- define signals in time step T1
-				CASE I IS
-				---
-				END CASE;
-			WHEN T2 => -- define signals in time step T2
-				CASE I IS
-				---
-				END CASE;
-			WHEN T3 => -- define signals in time step T3
-				CASE I IS
-				---
-				END CASE;
-		END CASE;
-	END PROCESS;
+	controlsignals: PROCESS (Tstep_Q, instruction, regX, regY)
+		BEGIN
+			-- specify initial values
+			CASE Tstep_Q IS
+				WHEN T0 => -- store DIN in IR as long as Tstep_Q = 0
+					IRin <= '1';
+				WHEN T1 => -- define signals in time step T1
+					CASE instruction IS
+					---
+					END CASE;
+				WHEN T2 => -- define signals in time step T2
+					CASE instruction IS
+					---
+					END CASE;
+				WHEN T3 => -- define signals in time step T3
+					CASE instruction IS
+					---
+					END CASE;
+			END CASE;
+		END PROCESS;
 
 	fsmflipflops: PROCESS (Clock, Resetn)
-	BEGIN
-		---
-	END PROCESS;
+		BEGIN
+			IF (Resetn = '1') THEN
+				Tstep_Q <= T0;
+			ELSIF (Clock'event AND Clock = '1') THEN
+				Tstep_Q <= Tstep_D;
+			END IF;
+		END PROCESS;
 	
-	reg_0: work.regn PORT MAP (BusWires, Rin(0), Clock, R0);
-	-- instantiate registers and the adder/subtracter unit
-	-- define the bus
-
 END Mixed;
