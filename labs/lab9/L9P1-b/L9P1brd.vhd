2a85d816da621efb96c44b464ac76a992bcc76cb
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-09 22:41:04 +1100

    it werk

--- labs/lab9/L9P1-b/L9P1brd.vhd
+++ labs/lab9/L9P1-b/L9P1brd.vhd
@@ -3,20 +3,22 @@ USE ieee.std_logic_1164.all;
 
 ENTITY L9P1brd IS
 	PORT (
-		KEY	: IN	std_logic_vector(9 DOWNTO 0);
-		SW		: IN	std_logic_vector(1 DOWNTO 0);
+		KEY	: IN	std_logic_vector(1 DOWNTO 0);
+		SW		: IN	std_logic_vector(9 DOWNTO 0);
 		LEDR	: OUT std_logic_vector(9 DOWNTO 0)
 	);
 END L9P1brd;
 
 ARCHITECTURE implementation OF L9P1brd IS
-
+	SIGNAL Clk : STD_LOGIC;
 BEGIN
+	Clk <= NOT(KEY(1));
+	
 	proc: work.L9P1proc PORT MAP (
-			DIN => KEY(8 DOWNTO 0),
-			Resetn => SW(0),
-			Clock => SW(1),
-			Run => KEY(9),
+			DIN => SW(8 DOWNTO 0),
+			Resetn => KEY(0),
+			Clock => Clk,
+			Run => SW(9),
 			Done => LEDR(9),
 			BusWires => LEDR(8 DOWNTO 0)
 		);
